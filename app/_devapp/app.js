ff7392fe9e4f7aec8e32f1a22dde01e2493d4b32
John Delos Reyes <jccdelosreyes.official@gmail.com>
2019-12-27 16:13:51 +0800

    no message

---
+++ app/_devapp/app.js
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
+import { asyncComponent } from 'react-async-component';
+
+/** We are importing our index.php my app Vairaible */
+import myApp from 'myApp';
+
+class Myapp extends Component {
+    render() {
+
+        const { user : { name, email }, logged } = myApp;
+
+        return (
+            <Fragment>
+                <div className="dashboard">
+                    {logged &&
+                        <h2 className="status">Logged In</h2>
+                    }
+                    <h1 className="name"> {name}</h1>
+                    <p className="email">{email}</p>
+
+                    <p>API host variable {__API_HOST__}</p>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+render(<Myapp/>, document.getElementById('app'));
